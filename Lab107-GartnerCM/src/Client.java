313aa07a8224fef189586abc80127e5464033ba3
FerisJumbo <colegartner1@student.isd423.org>
2021-10-15 10:24:30 -0500

    Created lab107 project

---
+++ Lab107-GartnerCM/src/Client.java
@@ -0,0 +1,15 @@
+/**
+ * Client class
+ * @author Cole Gartner
+ * @version Oct 15, 2021
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
